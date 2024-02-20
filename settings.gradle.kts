e651aaa6152dda0c2b622709569ceda52299cfe8
Петров Николай Владимирович (4090521) <petrovn@dev.vtb.ru>
2024-02-21 02:07:14 +0300

    - update kubernetes files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
+pluginManagement {
+    repositories {
+        maven {
+            name 'maven-central'
+            url "https://nexus.codikup-qa.com/repository/maven-central/"
+            credentials {
+                username "admin"
+                password "uhVUQ0IHE4Bf12fcD"
+            }
+        }
+}
+
 rootProject.name = "develop"
